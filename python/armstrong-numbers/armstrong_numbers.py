a31e6474f1b68c50d678dccb2ad14bcd90625bdc
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-08-24 18:22:01 +0530

    exercism Extra 06: Minor optimizations

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,5 +1,5 @@
 def is_armstrong_number(number):
-    digs = list(map(int, list(str(number))))
+    digs = list(map(int, str(number)))
     n_digs = len(digs)
     ans = sum(map(lambda base: base ** n_digs, digs))
     return ans == number
