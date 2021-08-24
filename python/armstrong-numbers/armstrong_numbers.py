88ca501a42aefc25c5e47b7f185b1de9c410288e
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-08-24 17:57:40 +0530

    exercism Extra 06: Solved armstrong-numbers

---
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -0,0 +1,5 @@
+def is_armstrong_number(number):
+    digs = list(map(int, list(str(number))))
+    n_digs = len(digs)
+    ans = sum(map(lambda base: base ** n_digs, digs))
+    return ans == number
