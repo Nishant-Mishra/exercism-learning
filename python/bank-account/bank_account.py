87bc0003f3baac09ac7d9d339234a89527c98081
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-08-24 18:24:09 +0530

    exercism Extra 05: Some cosmetic changes

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -3,14 +3,12 @@ from queue import SimpleQueue
 
 
 class BankAccount:
-
-    str_acc_not_open = "Account not Open/Active !!"
-    str_acc_already_open = "Account is already Open/Active !!"
-    str_acc_already_closed = "Account is already Closed !!"
-
     def __init__(self):
         self._balance = None
         self._mutex = Lock()
+
+    def __del__(self):
+        self._balance is None or self.close()
 
     def _verify_active(self, exc_txt=None):
         if self._balance is None:
@@ -20,7 +18,8 @@ class BankAccount:
         if self._balance is not None:
             raise ValueError(exc_txt or "Account already Open !!")
 
-    def _validate_val(self, val):
+    @staticmethod
+    def _validate_val(val):
         if val < 0:
             raise ValueError("Invalid transaction !!")
 
@@ -29,11 +28,11 @@ class BankAccount:
         self._verify_active()
         return self._balance
 
-    def debit(self, val):
+    def _debit(self, val):
         self._validate_val(val)
         self._update_balance(-val)
 
-    def credit(self, val):
+    def _credit(self, val):
         self._validate_val(val)
         self._update_balance(val)
 
@@ -52,16 +51,14 @@ class BankAccount:
 
     def deposit(self, amount):
         with self._mutex:
-            self.credit(amount)
+            self._credit(amount)
 
     def withdraw(self, amount):
         with self._mutex:
-            self.debit(amount)
+            self._debit(amount)
 
     def close(self):
         self._verify_active("Account already closed !!")
         with self._mutex:
             self._balance = None
 
-    def __del__(self):
-        self._balance is None or self.close()
