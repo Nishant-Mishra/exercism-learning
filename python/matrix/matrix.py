db93fe8dea97305df28bf42a0bc78a6df5b94563
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-08-29 17:59:52 +0530

    Exercise 05: Changes after mentor review

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,17 +1,19 @@
+from typing import List
+
+
 class Matrix:
     def __init__(self, matrix_string: str):
-        lines = matrix_string.split('\n')
+        lines = matrix_string.splitlines()
         self._nums = [list(map(int, line.split())) for line in lines]
         self._colsz = len(self._nums[0])
         self._rowsz = len(self._nums)
-        self._elems = self._rowsz * self._colsz
 
-    def row(self, index: int):
+    def row(self, index: int) -> List[int]:
         if index > self._rowsz:
-            raise ValueError(f"Invalid index '{index}'")
+            raise IndexError(f"Invalid index '{index}'")
         return self._nums[index - 1]
 
-    def column(self, index: int):
+    def column(self, index: int) -> List[int]:
         if index > self._colsz:
-            raise ValueError(f"Invalid index '{index}'")
-        return [self._nums[i][index - 1] for i in range(self._rowsz)]
+            raise IndexError(f"Invalid index '{index}'")
+        return [row[index - 1] for row in self._nums]
