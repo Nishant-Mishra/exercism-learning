5f964f337afc2429c57244a41f97d9282f134ba7
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-09-05 20:08:46 +0530

    Exercise 28: Solved 'reverse-string'.

---
+++ python/reverse-string/reverse_string.py
@@ -0,0 +1,15 @@
+"""
+Reverse a string
+
+For example: input: "cool" output: "looc"
+
+"""
+
+
+def reverse(text: str) -> str:
+    """
+
+    :param text: str: Input string
+    :return: str: Reversed string
+    """
+    return text[::-1]
