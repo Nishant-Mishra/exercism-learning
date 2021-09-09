4ad083e875fc4a671929fbf1f2496bf83b9e4e59
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-09-09 23:13:53 +0530

    Exercise 21: 'isbn-verifier': Changes after Mentor Review.
    Use recursive approach, as for this problem, it is more readable

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -5,6 +5,7 @@ calculating the check digit for the ISBN.
 
 The program should be able to verify ISBN-10 both with and without separating dashes.
 """
+from typing import List
 
 MOD_ISBN10 = 11
 
@@ -15,19 +16,45 @@ def is_valid(isbn: str) -> bool:
     :param isbn: str - An ISBN string to be validated
     :return: bool - Whether given ISBN string is valid or not
     """
-    isbn = isbn.replace('-', '')
-    if len(isbn) != 10:
+    return is_valid_recursive(list(isbn), 0, 10)
+
+
+def is_valid_recursive(isbn_ch_list: List[str], chksum: int, multiplier: int) -> bool:
+    """
+
+    :param isbn_ch_list: List of characters in the ISBN String
+    :param chksum: Checksum calculated for the already visited digits
+    :param multiplier: Currrent Multiplier
+    :return: bool: Whether the given ISBN is valid or not
+    """
+    # ISBN digits exhausted...
+    if not isbn_ch_list:
+        # And we checked 10 digits...
+        if multiplier == 0:
+            return chksum % MOD_ISBN10 == 0
+        # Less digits were supplied
         return False
 
-    try:
-        _sum = sum(int(dig) * (10 - i) for i, dig in enumerate(isbn[:-1]))
-    except ValueError:
+    first, *rest = isbn_ch_list
+
+    # Skip over '-'
+    if first == '-':
+        return is_valid_recursive(rest, chksum, multiplier)
+
+    # If we reached chksum digit with value '10'
+    if first == 'X':
+        # If this is the last digit
+        if not rest:
+            return is_valid_recursive(rest, chksum + 10, multiplier - 1)
+        # We found 'X' in middle
         return False
 
-    check_dig = isbn[-1].replace('X', '10')
-    try:
-        check_dig = int(check_dig)
-    except ValueError:
-        return False
+    # Calculate chksum and proceed...
+    if first.isdigit():
+        return is_valid_recursive(rest, chksum + multiplier * int(first), multiplier - 1)
+    # Found an alien character... Not Allowed !!
+    return False
 
-    return not bool((_sum + check_dig) % MOD_ISBN10)
+
+if __name__ == '__main__':
+    print(is_valid(""))
